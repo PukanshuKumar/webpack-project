146e7f6bfbb685fe76af6cb94d78d31bdee06a58
Pukanshu Kumar <pukanshukumar@gmail.com>
2023-07-22 23:50:54 +0530

    second phase completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShielRecipe = {
     refinemoonston:4,
 };
 
+const elvenGauntletsRecipe = {
+    ...elvenShielRecipe,
+    leather: 1,
+    refinedMoonstone:4,
+}
+
 console.log(elvenShielRecipe);
+console.log(elvenGauntletsRecipe);
