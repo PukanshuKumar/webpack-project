1a957543c4124deac6365e4aaf50d8e4390ca8c6
Pukanshu Kumar <pukanshukumar@gmail.com>
2023-07-23 18:08:05 +0530

    deployer error solve with use of cross-env

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,27 @@
-import "../styles/index.scss"
-import React from "react"
-import Recipes from "./Recipes"
-import sword from "../images/sword-img.avif"
+import "../styles/index.scss";
+import React, { lazy, Suspense } from "react";
+import sword from "../images/sword-img.avif";
+
+// Use lazy loading for the Recipes component
+const Recipes = lazy(() => import("./Recipes"));
 
 const App = () => {
-    return (
-        <>
-            <section className="hero"></section>
-            <main>
-            <section>
-                <h1>Oh hai, React</h1>
-            </section>
-                <img src={sword} alt="sword" width="250" />
-            <Recipes />
-            </main>
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>Oh hai, React</h1>
+        </section>
+        <img src={sword} alt="sword" width="250" />
 
-        </>
-    )
-}
+        {/* Wrap the Recipes component in Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Recipes />
+        </Suspense>
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
